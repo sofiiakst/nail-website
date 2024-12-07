fbab3ba89c032d24b3b1704be113cf81088c0b19
Sofia Kostopoulou <sophikos>
2024-12-07 22:18:15 +0200

    Add redirection logic for Instagram browser issue

--- app/open-in-browser/page.js
+++ app/open-in-browser/page.js
@@ -5,7 +5,7 @@ export default function OpenInBrowserPage() {
     <div style={{ textAlign: "center", padding: "20px" }}>
       <h1>Open in Browser</h1>
       <p>
-        It looks like you're trying to access this page from Instagram's in-app
+        It looks like youre trying to access this page from Instagrams in-app
         browser, which might block secure login.
       </p>
       <p>To continue, please open this link in your system browser:</p>
@@ -15,10 +15,10 @@ export default function OpenInBrowserPage() {
           icon).
         </li>
         <li>
-          Select <b>"Open in Browser"</b> from the menu.
+          Select <b>&quot;Open in Browser&quot;</b> from the menu.
         </li>
       </ol>
-      <p>After that, you’ll be able to log in securely.</p>
+      <p>After that, youll be able to log in securely.</p>
       <a href="https://nail-website-demo.vercel.app">
         <button
           style={{ padding: "10px 20px", fontSize: "16px", marginTop: "20px" }}
