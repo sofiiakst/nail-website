b1274c941e414b22ea78cf97f710171f4cc70310
Sofia Kostopoulou <sophikos>
2024-12-07 21:37:37 +0200

    Solving login through insta link

---
+++ app/components/Redirect.js
@@ -0,0 +1,22 @@
+"use client";
+
+import { useEffect } from "react";
+
+export default function RedirectToBrowser() {
+  useEffect(() => {
+    const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    if (
+      userAgent.includes("Instagram") ||
+      userAgent.includes("FBAN") ||
+      userAgent.includes("FBAV")
+    ) {
+      alert(
+        "You will be redirected to your default browser to log in securely."
+      );
+      window.location.href = "https://https://nail-website-demo.vercel.app"; // Replace with your app URL
+    }
+  }, []);
+
+  return null;
+}
